98b0827528c57d15b8e8e783a9097634273a5d27
sweet1230 <shwetariyal99@gmail.com>
2026-04-18 17:42:49 +0530

    new update

--- basic.js
+++ basic.js
@@ -1,5 +1,13 @@
 console.log("First Page ");
-let a,b;
-a="Shweta";
-b="Riyal";
-console.log(a+b);
+// let a,b;
+// a="Shweta";
+// b="Riyal";
+// console.log(a+b);
+const x=[];
+x[4]=1;
+x.forEach((i)=>{
+    console.log("hoo")
+})
+
+console.log("2">"10");
+console.log("2">10);
